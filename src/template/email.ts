6d89108b7bdd94428c89534f37994eecb70fc17f
iamrony777 <iamrony777@proton.me>
2023-06-03 20:10:28 +0530

    Add workflows/start.yml

--- src/template/email.ts
+++ src/template/email.ts
@@ -1,6 +1,6 @@
 import { ZONES } from "../handlers/mmi";
 
-
+// This Email Template is from https://github.com/Siddharth11/mmi-alert/blob/master/emailTemplate.js
 export const emailTemplate = (zone: string, mmi: number) => `
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head>
     <!--[if gte mso 9]><xml>
